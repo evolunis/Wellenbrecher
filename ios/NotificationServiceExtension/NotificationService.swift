c0a71989ac72273535c554fa52de0f74172fc997
Aloys Cretegny <aloys.cretegny@gmail.com>
2022-09-29 16:45:29 +0200

    test

--- ios/NotificationServiceExtension/NotificationService.swift
+++ ios/NotificationServiceExtension/NotificationService.swift
@@ -12,7 +12,7 @@ class NotificationService: UNNotificationServiceExtension {
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
-    public override public func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    public override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
@@ -27,7 +27,7 @@ class NotificationService: UNNotificationServiceExtension {
         }
     }
     
-    public override public func serviceExtensionTimeWillExpire() {
+    public override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
         if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
