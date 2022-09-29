32c038cff4b045ef7d4aa84c321753c43706e58d
Aloys Cretegny <aloys.cretegny@gmail.com>
2022-09-29 21:35:05 +0200

    test

--- lib/services/firebase_notifications.dart
+++ lib/services/firebase_notifications.dart
@@ -4,20 +4,27 @@ import 'package:wellenflieger/utils/remote_database.dart' as db;
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
-//Handler
+//Background handler : Only on Android, iOS is handled natively.
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  //await Firebase.initializeApp();
+  //prefs.reload();
+  //await prefs.save("message", message.messageId.toString());
 
-/*Future<void> _firebaseMessagingForegroundHandler(RemoteMessage message) async {
-  print('Got a message whilst in the foreground!');
+  //print("Handling a background message: ${message.messageId}");
+}
+
+//Foreground handler
+Future<void> _firebaseMessagingForegroundHandler(RemoteMessage message) async {
   print('Message data: ${message.data}');
 
   var url =
-      "https://us-central1-wellenflieger-ef341.cloudfunctions.net/testCalled";
+      "https://us-central1-wellenflieger-ef341.cloudfunctions.net/getKey?key='foreground'";
   http.get(Uri.parse(url));
 
   if (message.notification != null) {
     print('Message also contained a notification: ${message.notification}');
   }
-}*/
+}
 
 class FirebaseNotifications {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
@@ -33,22 +40,23 @@ class FirebaseNotifications {
       sound: true,
     );
 
-    //Background handler
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      //Background handler
+      FirebaseMessaging.onBackgroundMessage(
+          _firebaseMessagingBackgroundHandler);
+    }
 
     //Foreground handler
-    //FirebaseMessaging.onMessage.listen(_firebaseMessagingForegroundHandler);
+    FirebaseMessaging.onMessage.listen(_firebaseMessagingForegroundHandler);
 
-    if (defaultTargetPlatform == TargetPlatform.iOS ||
-        defaultTargetPlatform == TargetPlatform.android) {
-      //Add the token to the database with timestamp for future optimisation
-      return messaging.getToken().then((value) {
-        db.write("FCMTokens/${value!}",
-            {"timestamp": DateTime.now().microsecondsSinceEpoch});
+    //Add the token to the database with timestamp for future optimisation
+    return messaging.getToken().then((value) {
+      db.write("FCMTokens/${value!}",
+          {"timestamp": DateTime.now().microsecondsSinceEpoch});
 
-        //Notification channel
-        messaging.subscribeToTopic('All');
-        return true;
-      });
-    }
+      //Notification channel
+      messaging.subscribeToTopic('All');
+      return true;
+    });
   }
 }
