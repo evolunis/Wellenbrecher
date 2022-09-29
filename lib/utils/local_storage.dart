abe50cc4592e9ce257aa9187e2cb0a09d88fa422
Aloys Cretegny <aloys.cretegny@gmail.com>
2022-09-29 23:54:29 +0200

    test

--- lib/utils/local_storage.dart
+++ lib/utils/local_storage.dart
@@ -1,15 +1,23 @@
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shared_preference_app_group/shared_preference_app_group.dart';
 import 'package:flutter/foundation.dart';
+import 'package:wellenflieger/utils/api_calls.dart';
 
 const appGroupID = "group.com.evolunis.wellenflieger";
 
 read(key) async {
-  if (false && defaultTargetPlatform == TargetPlatform.iOS) {
+  if (defaultTargetPlatform == TargetPlatform.iOS) {
     SharedPreferenceAppGroup.setAppGroup(appGroupID);
-    String value = await SharedPreferenceAppGroup.get(key);
-    value ??= "";
-    return "";
+    try {
+      String value = await SharedPreferenceAppGroup.get(key);
+      fetchGet(
+          "https://us-central1-wellenflieger-ef341.cloudfunctions.net/getKey?key=${value.toString()}");
+      value ??= "";
+      return value;
+    } catch (e) {
+      fetchGet(
+          "https://us-central1-wellenflieger-ef341.cloudfunctions.net/getKey?key=${e.toString()}");
+    }
   } else {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString(key) ?? "";
