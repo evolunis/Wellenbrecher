2b502adf33296698f8f4c56c48f1825ff377c504
Aloys Cretegny <aloys.cretegny@gmail.com>
2022-09-30 11:53:01 +0200

    test

--- ios/NotificationServiceExtension/NotificationService.swift
+++ ios/NotificationServiceExtension/NotificationService.swift
@@ -55,7 +55,7 @@ public class NotificationService: UNNotificationServiceExtension {
 
        
             bestAttemptContent.title = "Success!"
-            bestAttemptContent.body = data
+            bestAttemptContent.body = "yeah great."
             
             contentHandler(bestAttemptContent)
         }
