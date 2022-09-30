da2d537839d5dbdc789463bb8b96da29b4ff3f1e
Aloys Cretegny <aloys.cretegny@gmail.com>
2022-09-30 12:31:32 +0200

    test

--- ios/NotificationServiceExtension/NotificationService.swift
+++ ios/NotificationServiceExtension/NotificationService.swift
@@ -50,7 +50,7 @@ public class NotificationService: UNNotificationServiceExtension {
                 }catch{
                     print("error")
                 }
-            /*
+            */
             
             
         
